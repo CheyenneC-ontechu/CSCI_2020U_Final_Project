8fbf8d499bb10af7cc61a3b5ff27207e2773be54
100708410-uoit <martin.truong@ontariotechu.net>
2020-03-22 21:51:26 -0400

    Refactor Ship.java. Also, please move this into src/sample.

--- src/Ship.java
+++ src/Ship.java
@@ -1,32 +1,32 @@
 public class Ship {
-    int spaces;
-    String name;
-    boolean sunk;
-    int[][] coordinates;
-    int hits;
+    private int spaces;
+    private String name;
+    private boolean sunk;
+    private int[][] coordinates;
+    private int hits;
+
     Ship(int spaces, String name){
         this.spaces = spaces;
         this.name = name;
-
         hits = 0;
         this.sunk = false;
         coordinates = new int[spaces][2];
     }
-    public boolean isHit(int[] coordinates, boolean[][] board) {
-        if (board[coordinates[0]][coordinates[1]]) {
-            return true;
-        }
-        return false;
-    }
+
+    //getters and setters
+    public int getSpaces() {return spaces;}
+    public String getName() {return name;}
     public int[][] getCoordinates() {return coordinates;}
-    public void setCoordinates(int[][] c) {
-        coordinates = c;
+    public void setCoordinates(int[][] coords) {coordinates = coords;}
+
+    //check if ship has been hit
+    public boolean isHit(int[] coordinates, boolean[][] board) {
+        return board[coordinates[0]][coordinates[1]];
     }
+
+    //check if ship has been sunk
     public boolean isSunk() {
-        if (hits == spaces) {
-            return true;
-        }
-        return false;
+        return hits == spaces;
     }
 
 }
